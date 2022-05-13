9b8fadc3455974299b47fb754d5281e26eaa4cd3
defu li <defu.ldf@alibaba-inc.com>
2022-05-13 23:08:11 +0800

    sort backend data records

--- pkg/models/data.go
+++ pkg/models/data.go
@@ -1,43 +1,10 @@
 package models
 
 import (
-	"encoding/json"
-	"github.com/grafana/grafana-plugin-sdk-go/backend"
-	"github.com/grafana/grafana-plugin-sdk-go/backend/log"
-	"strconv"
 	"time"
 )
 
 type DataRecord struct {
 	Time   time.Time
 	Values map[string]float64
-}
-
-func ParsePayload(query backend.DataQuery) (*QueryPayload, error) {
-	var payload QueryPayload
-
-	// Unmarshal the JSON into QueryPayload.
-	err := json.Unmarshal(query.JSON, &payload)
-	if err != nil {
-		return nil, err
-	}
-
-	payload.From = query.TimeRange.From.UnixMilli() / 1000
-	payload.To = query.TimeRange.To.UnixMilli() / 1000
-	payload.MaxDataPoints = query.MaxDataPoints
-	if len(payload.Format) == 0 {
-		payload.Format = "Table"
-	}
-	if len(payload.TimeField) == 0 {
-		payload.TimeField = "time"
-	}
-	if len(payload.Timezone) == 0 {
-		payload.Timezone = "Asia/Shanghai"
-	}
-	log.DefaultLogger.Info("ParsePayload", "payload.Query", payload.Query,
-		"format", payload.Format, "TimeField", payload.TimeField, "Timezone", payload.Timezone,
-		"from", strconv.FormatInt(payload.From, 10), "to", strconv.FormatInt(payload.To, 10),
-		"MaxDataPoints", payload.MaxDataPoints)
-
-	return &payload, nil
 }
